97b12b7ae887a10456c4a4c9bbcd8eab8f3ec951
JuanCaceffo <caceffojuan@gmail.com>
2023-05-29 01:46:47 -0300

    agregado de input de exactitud, cambio de funcion evaluada

--- secanteAndNewtonMethods.py
+++ secanteAndNewtonMethods.py
@@ -1,11 +1,14 @@
+import math as m
 def f(x):
-    return x**4
+    return m.sin(m.sqrt(((m.e**x)+3))/2)
 def f1(x):
-    return 4*x**3
+    return (m.e*m.cos(m.sqrt(((m.e**x)+3))/2))/(4*m.sqrt(((m.e**x)+3)))
 def printSteps(x1,x,pasos):
     print (f"paso : {pasos} -------- [x1,x] = {round(abs(x1-x),3)} -------- raizActual = {round(x1,3)} -------- f(raizActual) = {round(f(x1),3)}")
 
-def newton(x,epsilon):
+#x = valor inicial
+#tolerancia = valor del intervalo como temrino de parada de convergencia
+def newton(x,tolerancia):
     pasos = 0
     #anidamos funcion para poder utilizar variables no locales
     def secante():
@@ -21,7 +24,7 @@ def newton(x,epsilon):
         except ZeroDivisionError:
             print(f"error: f'({x}) = 0, no se puede dividir por 0")
             exit(6)
-        if (abs(x-x1) <= epsilon):
+        if (abs(x-x1) <= tolerancia):
             break
         x = secante()
         pasos += 1 
@@ -30,17 +33,27 @@ def newton(x,epsilon):
     
 def dataEntry():
     x = float(input("ingrese un numero cercano a la raiz buscada: "))
-    while (True):
-        epsilon = float(input("ingrese un intervalo de error aceptado: "))  
-        if(epsilon > 0):
-            break
-        else:
-            print("ingrese un epsilon positivo")    
-    return (x,epsilon)
+    # Pedir al usuario la cantidad de decimales correctos
+    #rango de exactitud de decimales posible
+    RANGO_TOLERABLE = range(1,11)
+    exactitud = int(input(f"ingrese la cantidad de decimales correctos que desea, (deben estar dentro del rango ({RANGO_TOLERABLE.start}..{RANGO_TOLERABLE.stop-1}): "))
+    #verificar que la exactitud ingresada por el usuario este detro del rango
+    while (not(exactitud in RANGO_TOLERABLE)):    
+        exactitud = int(input(f"ingrese la cantidad de decimales correctos que desea, (deben estar dentro del rango ({RANGO_TOLERABLE.start}..{RANGO_TOLERABLE.stop-1}): "))
+    #scamos el valor de paradad dada la exactitud
+    tolerancia = "0."
+    #agregamos tantos 0 como exactitud indicada
+    for i in range(exactitud): tolerancia += "0"
+    #agregamos un uno para indicar que es una fraccion
+    tolerancia += "1"
+    #convertimos a flotante para poder operar
+    tolerancia = float(tolerancia)
+
+    return (x,tolerancia,exactitud)
 
 def main():
-    (x,epsilon) = dataEntry()
+    (x,epsilon,exactitud) = dataEntry()
     raiz = newton(x,epsilon)
-    print(f"raiz aproximada con error menor a epsilon = {raiz}")
+    print(f"raiz aproximada con una exactitud de {exactitud} decimales = {round(raiz,exactitud)}")
 
 main()  
