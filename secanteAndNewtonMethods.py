63064a1d785298a5da26e35b19f09f3ab57d5a0c
JuanCaceffo <caceffojuan@gmail.com>
2023-05-29 13:47:09 -0300

    agregado de foto de ejemplo y comentarios en codigo fuente

--- secanteAndNewtonMethods.py
+++ secanteAndNewtonMethods.py
@@ -1,6 +1,18 @@
 import math as m
 import matplotlib.pyplot as plt
 import numpy as np
+"""
+El objetivo de este tp es aproximar raíces combinando el método Secante más el de Newton Raphson 
+intercalando entre uno y otro, a mi equipo y a mi nos pareció más interesante comenzar con el método 
+de Newton Raphson porque solo necesitamos pedirle al usuario un número inicial. 
+El programa pide al usuario un número cercano a la raíz y la cantidad de decimales exactos que desa. 
+Con estos datos el programa realiza la cantidad de iteraciones que necesite para conseguir un número(raíz) 
+que tenga los decimales exactos indicados por el usuario. El lenguaje de programación seleccionado es 
+python 3.10.6, las funciones a las que se desea someter el método tienen que ser asignadas en el código
+fuente y verificar que cumplan con las condiciones que pide cada método si desea garantizar la convergencia.
+
+por defecto tiene seteada la funcion función e^(-x)-sin(x) con su derivada -cos(x)-e^(-x)
+"""
 
 def f(x):
     return m.exp(-x)-m.sin(x)
